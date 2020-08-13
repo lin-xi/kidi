453a5ca81a557d0e2bcba9b4cbfc44c9e90d0593
lin-xi <mcnjqe@163.com>
2020-08-13 14:52:50 +0800

    update

--- test/service.test.js
+++ test/service.test.js
@@ -1,95 +1,97 @@
-import fetch from 'node-fetch';
-import {app, router, model, service} from '../lib/index.js';
+import fetch from "node-fetch";
+import { app, router, model, service } from "../lib/index.js";
 
 app.run(6000, {
-    "staticPath": "test/static",
-    "uploadPath": "test/upload",
-    "database": {
-        "type": "sqlite",
-        "database": "test/data/test.db",
-        "logging": true,
-        "synchronize": true,
-    },
+  staticPath: "test/static",
+  uploadPath: "test/upload",
+  database: {
+    type: "sqlite",
+    database: "test/data/test.db",
+    logging: true,
+    synchronize: true,
+  },
 });
 
 let userModel = model.create("userModel", {
-    id: {
-        type: Number,
-        primary: true,
-        generated: true,
-    },
-    name: {
-        type: String,
-    }, //资源配置
+  id: {
+    type: Number,
+    primary: true,
+    generated: true,
+  },
+  name: {
+    type: String,
+  }, //资源配置
 });
 
 // inject model
 @service("user", "userModel")
 class UserService {
-    constructor(userModel) {
-        this.model = userModel;
-    }
-    add(user) {
-        return this.model.add(user);
-    }
-    find(data) {
-        return this.model.find({ where: data });
-    }
+  constructor(userModel) {
+    this.model = userModel;
+  }
+  add(user) {
+    return this.model.add(user);
+  }
+  find(data) {
+    return this.model.find({ where: data });
+  }
 }
 
 // inject service
 @service("user2", "user")
 class UserService2 {
-    constructor(user) {
-        this.service = user;
-    }
-    find2(data) {
-        return this.service.find(data);
-    }
+  constructor(user) {
+    this.service = user;
+  }
+  find2(data) {
+    return this.service.find(data);
+  }
 }
 
 router.get("/test/service", async (ctx, res, next, services) => {
-    let user = services.user;
-    let user2 = services.user2;
-    let record = await user.add({name: "kidi-service-new"});
+  let user = services.user;
+  let user2 = services.user2;
+  let record = await user.add({ name: "kidi-service-new" });
 
-    let records = await user2.find2({name: "kidi-service-new"});
-    if(records.length > 0) {
-        res.json({"result": records[0].name})
-    } else {
-        res.json({"result": "none"})
-    }
-})
+  let records = await user2.find2({ name: "kidi-service-new" });
+  if (records.length > 0) {
+    res.json({ result: records[0].name });
+  } else {
+    res.json({ result: "none" });
+  }
+});
 
 function pending() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        resolve();
-        }, 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
 }
 
 // service--------------------------
 describe("service", () => {
-    beforeEach(async () => {
-        await pending();
-    });
+  beforeEach(async () => {
+    await pending();
+  });
 
-    test('test service: di', async () => {
-        let result = await fetch("http://localhost:6000/test/service").then(res => res.json());
-        expect(result.result).toBe("kidi-service-new");
-    });
+  test("test service: di", async () => {
+    let result = await fetch("http://localhost:6000/test/service").then((res) =>
+      res.json()
+    );
+    expect(result.result).toBe("kidi-service-new");
+  });
 
-    test('test service: di error', async () => {
-        class Test {
-            constructor(hello) {
-                console.log(hello);
-            }
-        }
-        try{
-            service.create('user3', Test, 'hello');
-        } catch(err) {
-            expect(err.message).toBe("hello 依赖注入失败")
-        }
-    });
-})
+  test("test service: di error", async () => {
+    class Test {
+      constructor(hello) {
+        console.log(hello);
+      }
+    }
+    try {
+      service.create("user3", Test, "hello");
+    } catch (err) {
+      expect(err.message).toBe("hello 依赖注入失败");
+    }
+  });
+});
