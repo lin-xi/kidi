9019b7c69c7cb45530ec8a279578acd37119876c
lin-xi <mcnjqe@163.com>
2020-08-14 19:10:14 +0800

    build

---
+++ src/response.js
@@ -0,0 +1,58 @@
+export default class Response {
+    constructor(app, ctx) {
+        this.app = app;
+        this.ctx = ctx;
+        this.response = ctx.response;
+    }
+
+    status(code) {
+        this.ctx.status = code;
+        return this;
+    }
+
+    json(data) {
+        this.response.type = 'application/json';
+        this.ctx.status = 200;
+        this.response.body = JSON.stringify(data);
+    }
+
+    jsonp(callback, data) {
+        this.response.type = 'text/javascript';
+        this.ctx.status = 200;
+        this.response.body = `;${callback}(${JSON.stringify(data)})`
+    }
+
+    type(type) {
+        this.ctx.type = type;
+        return this;
+    }
+
+    attachment(filename) {
+        this.response.attachment(filename);
+        return this;
+    }
+
+    append(field, val) {
+        this.response.append(field, val);
+        return this;
+    }
+
+    set() {
+        this.response.set(...arguments);
+        return this;
+    }
+
+    get(field) {
+        return this.response.get(field);
+    }
+
+    cookie(name, value, options) {
+        this.ctx.cookies.set(name, value, options);
+        return this;
+    }
+
+    redirect(url, alt) {
+        this.response.redirect(url, alt);
+        return this;
+    }
+}
